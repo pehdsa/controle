04b4db6ed3a97243260f4eeba8264f08ddce41b0
InfoShop <infoshop@iMac-de-InfoShop.local>
2020-07-10 13:39:07 -0400

    mensagem

--- src/pages/Relatorios/index.js
+++ src/pages/Relatorios/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiShoppingBag, FiCheckCircle, FiClock } from "react-icons/fi";
+import { MdLocalAtm } from "react-icons/md";
 import HeaderComp from '../../components/headerComp';
 
 function Revendedores() {
@@ -7,7 +9,53 @@ function Revendedores() {
             
             <HeaderComp title="Relatorios" />
 
-            
+            <main className="conteudo container py-4">
+
+                <div className="relatorios">
+
+                    <div className="item d-flex align-items-center p-4">
+                        <div>
+                            <h2 className="font-28 line-height-100 primary-color">45</h2>
+                            <small className="font-11 line-height-120 default-color-6"><b>Pedidos em Aberto</b></small>
+                        </div>
+                        <div className="icone primary-color">
+                            <FiClock size={ 80 } />
+                        </div>
+                    </div>
+
+                    <div className="item d-flex align-items-center p-4">
+                        <div>
+                            <h2 className="font-28 line-height-100 primary-color">45</h2>
+                            <small className="font-11 line-height-120 default-color-6"><b>Pedidos Entregues</b></small>
+                        </div>
+                        <div className="icone primary-color">
+                            <FiCheckCircle size={ 80 } />
+                        </div>
+                    </div>
+
+                    <div className="item d-flex align-items-center p-4">
+                        <div>
+                            <h2 className="font-28 line-height-100 primary-color">45</h2>
+                            <small className="font-11 line-height-120 default-color-6"><b>Máscaras</b></small>
+                        </div>
+                        <div className="icone primary-color">
+                            <FiShoppingBag size={ 80 } />
+                        </div>
+                    </div>
+
+                    <div className="item d-flex align-items-center p-4">
+                        <div>
+                            <h2 className="font-24 line-height-100 primary-color"><small>R$</small> 45.000,00</h2>
+                            <small className="font-11 line-height-120 default-color-6"><b>Total Vendido</b></small>
+                        </div>
+                        <div className="icone primary-color">
+                            <MdLocalAtm size={ 80 } />
+                        </div>
+                    </div>
+
+                </div>
+
+            </main>
 
         </React.Fragment>
     )
